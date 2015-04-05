dafd1ff92089c4f4120dc8587cc8997d34036b70
fire855 <thefire855@gmail.com>
2015-04-05 12:59:36 +0200

    update device name
    Change-Id: I80910fc3b354a939e01caa52d702184103245398

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := mt6592
+PRODUCT_RELEASE_NAME := K1_turbo
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -13,6 +13,6 @@ $(call inherit-product, device/mediatek/mt6592/device_mt6592.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mt6592
 PRODUCT_NAME := cm_mt6592
-PRODUCT_BRAND := mediatek
-PRODUCT_MODEL := mt6592
-PRODUCT_MANUFACTURER := mediatek
+PRODUCT_BRAND := KINGZONE
+PRODUCT_MODEL := K1_turbo
+PRODUCT_MANUFACTURER := KINGZONE

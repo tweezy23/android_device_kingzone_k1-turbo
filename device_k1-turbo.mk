3ddd89e0a97e4f33dd2a51a872ec0a73e46be92d
fire855 <thefire855@gmail.com>
2015-05-15 21:14:28 +0200

    add torch
    Change-Id: I673236b1041ca991c7af5b208409aa1e22038c11

--- device_k1-turbo.mk
+++ device_k1-turbo.mk
@@ -81,6 +81,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+PRODUCT_PACKAGES += \
+    Torch
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
